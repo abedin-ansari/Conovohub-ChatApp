e31041a1e2d3449828a276c8686c12ec6a8c52fe
abedin-ansari <128270898+abedin-ansari@users.noreply.github.com>
2026-08-28 22:38:23 +0530

    css fix

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,52 +1,55 @@
-
-import React, { useEffect } from 'react'
-import SendMessage from './SendMessage'
-import Messages from './Messages';
-import { useSelector,useDispatch } from "react-redux";
-import { setSelectedUser } from '../utils/userSlice';
-import { IoArrowBack } from 'react-icons/io5';
+import React, { useEffect } from "react";
+import SendMessage from "./SendMessage";
+import Messages from "./Messages";
+import { useSelector, useDispatch } from "react-redux";
+import { setSelectedUser } from "../utils/userSlice";
+import { IoArrowBack } from "react-icons/io5";
 
 const MessageContainer = () => {
-    const { selectedUser, authUser, onlineUsers } = useSelector(store => store.user);
-    const dispatch = useDispatch();
+  const { selectedUser, authUser, onlineUsers } = useSelector(
+    (store) => store.user,
+  );
+  const dispatch = useDispatch();
 
+  const isOnline = onlineUsers?.includes(selectedUser?._id);
 
-const isOnline = onlineUsers?.includes(selectedUser?._id);
-   
-    return (
-        <>
-            {
-                selectedUser !== null ? (
-                    <div className='md:min-w-[650px] lg:min-w-[750px] xl:min-w-[850px] flex flex-col h-full min-h-0'>
-                        <div className='flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2'>
-                            <button onClick={() => dispatch(setSelectedUser(null))} aria-label='Back to users' title='Back to users' className='p-2 cursor-pointer'>
-                                <IoArrowBack className='w-5 h-5' />
-                            </button>
-                            <div className={`avatar ${isOnline ? 'online' : ''}`}>
-                                <div className='w-12 rounded-full'>
-                                    <img src={selectedUser?.profilePhoto} alt="user-profile" />
-                                </div>
-                            </div>
-                            <div className='flex flex-col flex-1'>
-                                <div className='flex justify-between gap-2'>
-                                    <p>{selectedUser?.fullName}</p>
-                                </div>
-                            </div>
-                        </div>
-                        <Messages />
-                        <SendMessage />
-                    </div>
-                ) : (
-                    <div className='md:min-w-[650px] lg:min-w-[750px] xl:min-w-[850px] flex flex-col justify-center items-center'>
-                        <h1 className='text-4xl text-white font-bold'>Hi,{authUser?.fullName} </h1>
-                        <h1 className='text-2xl text-white'>Let's start conversation</h1>
+  return (
+    <>
+      {selectedUser !== null ? (
+        <div className="md:min-w-[650px] lg:min-w-[750px] xl:min-w-[850px] flex flex-col h-full min-h-0">
+          <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2">
+            <button
+              onClick={() => dispatch(setSelectedUser(null))}
+              aria-label="Back to users"
+              title="Back to users"
+              className="p-2 cursor-pointer"
+            >
+              <IoArrowBack className="w-5 h-5" />
+            </button>
+            <div className={`avatar ${isOnline ? "online" : ""}`}>
+              <div className="w-12 rounded-full">
+                <img src={selectedUser?.profilePhoto} alt="user-profile" />
+              </div>
+            </div>
+            <div className="flex flex-col flex-1">
+              <div className="flex justify-between gap-2">
+                <p>{selectedUser?.fullName}</p>
+              </div>
+            </div>
+          </div>
+          <Messages />
+          <SendMessage />
+        </div>
+      ) : (
+        <div className="md:min-w-[650px] lg:min-w-[750px] xl:min-w-[850px] flex flex-col justify-center items-center">
+          <h1 className="text-4xl text-white font-bold">
+            Hi,{authUser?.fullName}{" "}
+          </h1>
+          <h1 className="text-2xl text-white">Let's start conversation</h1>
+        </div>
+      )}
+    </>
+  );
+};
 
-                    </div>
-                )
-            }
-        </>
-
-    )
-}
-
-export default MessageContainer
+export default MessageContainer;
